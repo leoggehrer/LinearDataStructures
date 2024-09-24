41cc1b042824636e94905a4131d828473481c371
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2024-09-24 20:04:30 +0200

    Changed _top to first and...

--- LinearDataStructures.Logic/Queue.cs
+++ LinearDataStructures.Logic/Queue.cs
@@ -1,101 +1,64 @@
 namespace LinearDataStructures.Logic
 {
-    /// <summary>
-    /// Represents a generic queue data structure that allows for adding and removing elements in a first-in, first-out (FIFO) manner.
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the queue.</typeparam>
     internal class Queue<T> : IQueue<T>
     {
         #region fields
-        private Element<T>? _top = null;
-        private Element<T>? _bottom = null;
+        private Element<T>? _first = null;
+        private Element<T>? _last = null;
         #endregion fields
 
-        #region properties
-        /// <summary>
-        /// Gets the number of elements in the collection.
-        /// </summary>
-        /// <value>
-        /// The total count of elements. This property traverses the entire collection to calculate the count.
-        /// </value>
-        /// <remarks>
-        /// This property has a time complexity of O(n), where n is the number of elements in the collection.
-        /// </remarks>
         public int Count
         {
             get
             {
                 int result = 0;
-                Element<T>? current = _top;
+                Element<T>? run = _first;
 
-                while (current != null)
+                while (run != null)
                 {
                     result++;
-                    current = current.Next;
+                    run = run.Next;
                 }
-
                 return result;
             }
         }
-        #endregion properties
 
-        #region methods
-        /// <summary>
-        /// Clears the contents of the data structure by setting the top and bottom references to null.
-        /// </summary>
         public void Clear()
         {
-            _top = null;
-            _bottom = null;
+            _first = null;
+            _last = null;
         }
 
-        /// <summary>
-        /// Removes and returns the value at the front of the queue.
-        /// </summary>
-        /// <returns>
-        /// The value at the front of the queue.
-        /// </returns>
-        /// <exception cref="System.InvalidOperationException">
-        /// Thrown when the queue is empty.
-        /// </exception>
         public T Dequeue()
         {
-            if (_top == null)
-            {
-                throw new System.InvalidOperationException("The queue is empty.");
-            }
+            if (_first == null)
+                throw new InvalidOperationException("The queue is empty!");
 
-            T value = _top.Value;
+            Element<T>? removeElement = _first;
 
-            _top = _top.Next;
-            if (_top == null)
+            _first = _first.Next;
+
+            if (_first == null)
             {
-                _bottom = null;
+                _last = null;
             }
-            return value;
+            return removeElement.Data;
         }
 
-        /// <summary>
-        /// Adds an item to the end of the queue.
-        /// </summary>
-        /// <param name="item">The item to be added to the queue.</param>
-        /// <remarks>
-        /// If the queue is empty, the new item becomes both the top and bottom of the queue.
-        /// If the queue is not empty, the new item is added to the end of the queue.
-        /// </remarks>
         public void Enqueue(T item)
         {
-            if (_top == null)
+            if (_first == null)
             {
-                _top = new Element<T>(item, null);
-                _bottom = _top;
+                _first = new Element<T>(item, null);
+                _last = _first;
             }
             else
             {
-                _bottom!.Next = new Element<T>(item, null);
-                _bottom = _bottom!.Next;
+                Element<T> newElement = new Element<T>(item, null);
+
+                _last!.Next = newElement;
+                _last = newElement;
             }
         }
-        #endregion methods
     }
 }
